55b54a166f9565d0389a864feac35ce14cb09e3d
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-03-05 15:27:23 +0530

    commit

---
+++ flashlight/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:torch_light/torch_light.dart';
+
+void main() {
+  runApp(TorchApp());
+}
+
+class TorchApp extends StatefulWidget {
+  @override
+  _TorchAppState createState() => _TorchAppState();
+}
+
+class _TorchAppState extends State<TorchApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      supportedLocales: const [Locale('en', '')],
+      // localizationsDelegates: const [
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      // ],
+      home: TorchController(),
+    );
+  }
+}
+
+class TorchController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('torch_light example app'),
+      ),
+      body: FutureBuilder<bool>(
+        future: _isTorchAvailable(context),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.hasData && snapshot.data!) {
+            return Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: ElevatedButton(
+                      child: const Text('Enable torch'),
+                      onPressed: () async {
+                        _enableTorch(context);
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: ElevatedButton(
+                      child: const Text('Disable torch'),
+                      onPressed: () {
+                        _disableTorch(context);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else if (snapshot.hasData) {
+            return const Center(
+              child: Text('No torch available.'),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Future<bool> _isTorchAvailable(BuildContext context) async {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    try {
+      return await TorchLight.isTorchAvailable();
+    } on Exception catch (_) {
+      scaffoldMessenger.showSnackBar(
+        const SnackBar
+          (
+          content: Text('Could not check if the device has an available torch'),
+        ),
+      );
+      rethrow;
+    }
+  }
+
+  Future<void> _enableTorch(BuildContext context) async {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    try {
+      await TorchLight.enableTorch();
+    } on Exception catch (_) {
+      scaffoldMessenger.showSnackBar(
+        const SnackBar(
+          content: Text('Could not enable torch'),
+        ),
+      );
+    }
+  }
+
+  Future<void> _disableTorch(BuildContext context) async {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    try {
+      await TorchLight.disableTorch();
+    } on Exception catch (_) {
+      scaffoldMessenger.showSnackBar(
+        const SnackBar(
+          content: Text('Could not disable torch'),
+        ),
+      );
+    }
+  }
+}
