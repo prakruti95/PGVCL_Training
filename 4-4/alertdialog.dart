9761088edf8f20d3dc14e70306682a1b52eae5cd
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-04-04 11:15:51 +0530

    commit

--- 4-4/alertdialog.dart
+++ 4-4/alertdialog.dart
@@ -1,106 +1,69 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
-class BackButton1 extends StatelessWidget
-{
-  const BackButton1({super.key});
 
+class BackButton1 extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp
-      (
-        home:MyApp2(),
-      );
-
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
-class MyApp2 extends StatefulWidget
-{
-  const MyApp2({super.key});
 
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyApp2> createState() => _MyApp2State();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyApp2State extends State<MyApp2>
-{
-  DateTime? _lastPressedAt;
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? _lastPressedAt; // Track the last press time
 
   @override
-  Widget build(BuildContext context)
-  {
-    return WillPopScope
-      (
-        onWillPop:() async
-        {
-          DateTime now = DateTime.now();
-          if (_lastPressedAt == null || now.difference(_lastPressedAt!) > Duration(seconds: 2))
-          {
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        DateTime now = DateTime.now();
+        if (_lastPressedAt == null ||
+            now.difference(_lastPressedAt!) > Duration(seconds: 2)) {
+          _lastPressedAt = now;
+          _showExitDialog();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Double Tap to Exit'),
+        ),
+        body: Center(
+          child: Text('Double Tap Back Button to Exit'),
+        ),
+      ),
+    );
+  }
 
-            _lastPressedAt = now;
-            _showExitDialog();
-
-            return false;
-          }
-
-
-            return true;
-        },
-
-        child: Scaffold
-          (
-          appBar: AppBar(title: Text("Double Tap to Exit"),),
-              body: Center
-                (
-                  child: Text("Double tap to exit"),
-                ),
+  void _showExitDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Exit App'),
+        content: Text('Press back again to exit the app.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
           ),
-
-
-      );
-
-
-    // return Scaffold
-    //   (
-    //     appBar: AppBar(title: Text("Double Tap to Exit"),),
-    //     body: Center
-    //       (
-    //         child: Text("Double tap to exit"),
-    //       ),
-    //
-    //   );
-  }
-
-void _showExitDialog()
-  {
-       showDialog
-        (
-          context: context,
-          builder: (context) => AlertDialog
-            (
-                title: Text("Are you sure you want to exit?"),
-                content:Text("if you want to exit directly press double tap"),
-                actions:
-                [
-                    TextButton(onPressed: ()
-                    {
-                        exit(0);
-
-                    }, child:Text("OK")),
-                    TextButton(onPressed: ()
-                    {
-
-                        Navigator.pop(context);
-
-                    }, child:Text("CANCEL"))
-
-                ],
-
-            )
-
-        );
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // Close the dialog
+              Navigator.of(context).maybePop(); // Exit the app
+            },
+            child: Text('Exit'),
+          ),
+        ],
+      ),
+    );
   }
 }
-
