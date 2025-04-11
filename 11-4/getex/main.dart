c974ec969afd315f2d459ac7ef8b62280db3f096
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-04-11 15:03:14 +0530

    commit

---
+++ 11-4/getex/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:getxex/homescreen.dart';
+import 'package:getxex/secondscreen.dart';
+import 'package:provider/provider.dart';
+
+import 'counter_provider.dart';
+
+void main()
+{
+  runApp
+    (
+      MultiProvider
+        (
+          providers:
+        [
+          ChangeNotifierProvider(create: (_) => CounterProvider()),
+        ],child: MyApp()),);
+
+}
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return GetMaterialApp
+      (
+        title: 'GetX + Provider',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        getPages:
+        [
+        GetPage(name: '/', page: () => HomeScreen()),
+        GetPage(name: '/second', page: () => SecondScreen()),
+      ],
+      );
+  }
+
+}
